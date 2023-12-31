bd76788c521a8eb5dd0228243c95c7fd18f8b0b1
Abhayjit Sodhi <67168409+AJDevCode@users.noreply.github.com>
2023-12-30 19:59:12 -0500

    Add files via upload

---
+++ StockMarket-Predictor/pythonProject/main.py
@@ -0,0 +1,111 @@
+# import statements
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pan
+
+import datetime as dt
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, LSTM
+import yahoo_fin as fin
+import yfinance as yf
+from pandas_datareader import data as pdr
+from datetime import datetime
+# Grabbing data to use in the stock program
+
+yf.pdr_override()
+company = 'SCHAND.NS'
+
+# start and end date to train data
+start = dt.datetime(2012, 1, 1)
+end = dt.datetime(2020, 1, 1)
+
+data = pdr.get_data_yahoo(company, start, end)
+
+#data = web.get_data_yahoo('FB', start=start, end=end)
+
+
+# Preparing to use the data
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1, 1))
+prediction_days = 60
+x_train = []
+y_train = []
+
+#append
+for i in range(prediction_days, len(scaled_data)):
+    x_train.append(scaled_data[i-prediction_days:i, 0])
+    y_train.append(scaled_data[i, 0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+#Using the prepared data to build the model
+
+model = Sequential()
+
+model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(Dropout(0.2))
+
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+
+model.add(LSTM(units=50))
+model.add(Dropout(0.2))
+
+model.add(Dense(units=1))
+model.compile(optimizer='adam', loss='mean_squared_error')
+model.fit(x_train, y_train, epochs=25,batch_size=32)
+
+
+# testing
+
+test_start = dt.datetime(2020, 1, 1)
+test_end = dt.datetime.now()
+
+test_data = pdr.get_data_yahoo(company, test_start, test_end)
+actual_prices = test_data['Close'].values
+total_dataset = pan.concat((data['Close'], test_data['Close']), axis=0)
+
+model_inputs = total_dataset[len(total_dataset)-len(test_data)-prediction_days:].values
+model_inputs = model_inputs.reshape(-1, 1)
+
+model_inputs = scaler.transform(model_inputs)
+
+
+
+# predictions used on test data created
+
+x_test = []
+
+for i in range(prediction_days, len(model_inputs)):
+    x_test.append(model_inputs[i-prediction_days:i, 0])
+
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+predicted_prices = model.predict(x_test)
+predicted_prices = scaler.inverse_transform(predicted_prices)
+
+
+#plot the test
+
+plt.plot(actual_prices, color="black", label=f"Actual {company} Price")
+plt.plot(predicted_prices, color="green", label=f"Predicted {company} Price")
+
+plt.title(f"{company} Share Price")
+plt.xlabel('Time')
+plt.ylabel(f'{company} Share Price')
+plt.legend()
+plt.show()
+
+
+#predicting future date price
+
+real_data = [model_inputs[len(model_inputs) + 1 - prediction_days:len(model_inputs + 1), 0]]
+real_data = np.array(real_data)
+real_data = np. reshape(real_data, (real_data.shape[0], real_data.shape[1], 1))
+
+prediction = model.predict(real_data)
+prediction = scaler.inverse_transform(prediction)
+print(f"Prediction of next day stock price: {prediction}")
